f0967f2f7ed7e66881122ed58e6126419b21ef4e
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-06-19 18:11:08 -0500

    prepare pipeline deploy ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig, loadEnv } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  base: '/fisiweb/',
-})
+export default defineConfig(({ command, mode }) => {
+  console.log("command: ", command);
+  console.log("mode: ", mode);
+  const { BASE_URL, PORT } = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [svelte()],
+    base: mode === "production" ? BASE_URL : "/",
+    preview: {
+      port: PORT,
+      host: "localhost",
+    },
+    server: {
+      port: PORT,
+      host: "localhost",
+      open: true,
+    },
+  };
+});
