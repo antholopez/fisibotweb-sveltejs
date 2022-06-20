22db725f085f0fad87332f0039a6b7fb8015f11b
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-06-20 02:36:26 -0500

    fix ok pipeline

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -110,6 +110,7 @@
       <a
         href="/"
         class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+        use:link
       >
         <img src={logo} alt="" width="40" height="32" />
         <span class="fs-4">Fisibot</span>
@@ -119,7 +120,7 @@
         class="nav col-12 col-lg-auto me-lg-auto mb-2 ms-2 justify-content-center mb-md-0"
       >
         <li>
-          <a href="/" class="nav-link px-2 text-white active">Inicio</a>
+          <a href="/" class="nav-link px-2 text-white active" use:link>Inicio</a>
         </li>
       </ul>
 
