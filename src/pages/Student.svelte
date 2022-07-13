72f7288ebbc8e048472c5e4bdd0d54cf3112acf9
Anthony Lopez Vega <anthodev1195@gmail.com>
2022-07-13 17:38:38 -0500

    fix modal page student ok

--- src/pages/Student.svelte
+++ src/pages/Student.svelte
@@ -1,5 +1,41 @@
 <script>
   import Header from "./../components/Header.svelte";
+  import { Modal, ModalBody, ModalHeader } from "sveltestrap";
+  import { notify } from "./../utils/notification.js";
+
+  let open = false;
+  const toggle = () => (open = !open);
+
+  let student = {
+    code: "",
+    email: "",
+    names: "",
+    lastNames: "",
+  };
+  let loadingForm = false;
+  let disabledForm = true;
+
+  $: student.code && student.email && student.names && student.lastNames
+    ? (disabledForm = false)
+    : (disabledForm = true);
+
+  const createNewStudent = () => {
+    console.log("student: ", student);
+  };
+
+  const clearCourse = () => {
+    student.code = "";
+    student.email = "";
+    student.names = "";
+    student.lastNames = "";
+
+    disabledForm = true
+  };
+
+  const closeModal = () => {
+    clearCourse();
+    open = false;
+  };
 </script>
 
 <Header />
@@ -13,7 +49,9 @@
       />
     </div>
     <div class="col-4 d-md-flex justify-content-md-end">
-      <button class="btn btn-dark me-md-2">Agregar Estudiante</button>
+      <button class="btn btn-dark me-md-2" on:click={toggle}
+        >Agregar Estudiante</button
+      >
     </div>
   </div>
 
@@ -21,15 +59,19 @@
     <thead>
       <tr>
         <th scope="col">#</th>
+        <th scope="col">Código</th>
+        <th scope="col">Email</th>
         <th scope="col">Nombres</th>
         <th scope="col">Apellidos</th>
         <th scope="col">Imagen</th>
-        <th scope="col"></th>
+        <th scope="col" />
       </tr>
     </thead>
     <tbody>
       <tr>
         <th scope="row">1</th>
+        <td>14200195</td>
+        <td>anthony.lopez@unmsm.edu.pe</td>
         <td>Mark</td>
         <td>Otto</td>
         <td>@mdo</td>
@@ -40,6 +82,8 @@
       </tr>
       <tr>
         <th scope="row">2</th>
+        <td>14200195</td>
+        <td>anthony.lopez@unmsm.edu.pe</td>
         <td>Jacob</td>
         <td>Thornton</td>
         <td>@fat</td>
@@ -50,6 +94,8 @@
       </tr>
       <tr>
         <th scope="row">3</th>
+        <td>14200195</td>
+        <td>anthony.lopez@unmsm.edu.pe</td>
         <td>Larry the Bird</td>
         <td>Larry the Bird</td>
         <td>@twitter</td>
@@ -61,6 +107,71 @@
     </tbody>
   </table>
 </div>
+<Modal
+  isOpen={open}
+  backdrop="static"
+  {toggle}
+  class="modal-dialog modal-dialog-centered"
+  on:close={closeModal}
+>
+  <ModalHeader {toggle}>Agregar Estudiante</ModalHeader>
+  <ModalBody>
+    <form on:submit|preventDefault={createNewStudent}>
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          bind:value={student.code}
+        />
+        <label for="floatingInput">Código</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          bind:value={student.email}
+        />
+        <label for="floatingInput">Correo</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          bind:value={student.names}
+        />
+        <label for="floatingInput">Nombres</label>
+      </div>
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          bind:value={student.lastNames}
+        />
+        <label for="floatingInput">Apellidos</label>
+      </div>
+      <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          data-bs-dismiss="modal"
+          disabled={loadingForm}
+          on:click={closeModal}>Cancelar</button
+        >
+        <button type="submit" class="btn btn-success" disabled={disabledForm}
+          >{#if !loadingForm}Agregar{:else}Agregando...{/if}</button
+        >
+      </div>
+    </form>
+  </ModalBody>
+</Modal>
 
 <style>
 </style>
